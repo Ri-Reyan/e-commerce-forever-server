e9f0623431cd17cb780f977486917ea3681cf559
REYAN <152781255+Ri-Reyan@users.noreply.github.com>
2026-07-05 19:40:26 +0600

    Add health check endpoint for server status

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 app.use("/api", userRouter);
 app.use("/api", adminRouter);
 app.use("/api", adminPanelRouter);
